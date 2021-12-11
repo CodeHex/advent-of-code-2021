9bcedfcf8074d24aa969a431551c2c6cc1bd4092
Ben Ibinson <ben@ibinson.dev>
2021-12-11 16:16:46 +0000

    Simplify

--- day11/main.go
+++ day11/main.go
@@ -16,47 +16,27 @@ func main() {
 
 	// Represents the heights of the seabed
 	data := matrices.NewIntMatrixFromLines(lines, splitter, convert.FuncFor[int]())
-	octopi := Octopi(data)
+	octopi := NewOctopi(data)
+	octopi.RunSimulation()
 
-	step := 0
-	totalFlashes := 0
-	flashesAfter100Steps := 0
-	firstAllFlashedOccurred := false
-	stepWhenFirstAllFlashed := 0
-	octopiView100Steps := ""
-	octopiViewFirstAllFlashed := ""
-	// Keep progressing the octopi until we have at least reached 100 steps
-	// and we have seen all the octopi flash at the same time
-	for !(step > 100 && firstAllFlashedOccurred) {
-		step++
-		stepFlashes, allFlashed := octopi.ProgressStep()
-		totalFlashes += stepFlashes
-
-		if step == 100 {
-			flashesAfter100Steps = totalFlashes
-			octopiView100Steps = octopi.String()
-		}
-
-		if allFlashed && !firstAllFlashedOccurred {
-			firstAllFlashedOccurred = true
-			stepWhenFirstAllFlashed = step
-			octopiViewFirstAllFlashed = octopi.String()
-		}
-	}
-
-	fmt.Printf("[Part 1] After 100 steps, total flashes: %d\n%s", flashesAfter100Steps, octopiView100Steps)
-	fmt.Printf("[Part 2] Until all octopi flash, steps taken: %d\n%s", stepWhenFirstAllFlashed, octopiViewFirstAllFlashed)
+	fmt.Printf("[Part 1] After 100 steps, total flashes: %d\n", octopi.flashesAfter100Steps)
+	fmt.Printf("[Part 2] Until all octopi flash, steps taken: %d\n", octopi.firstSyncFlashStep)
 }
 
-type Octopi matrices.IntMatrix[int]
-
-func (o Octopi) m() matrices.IntMatrix[int] {
-	return matrices.IntMatrix[int](o)
+type Octopi struct {
+	step                 int
+	data                 matrices.IntMatrix[int]
+	flashesAfter100Steps int
+	firstSyncFlashStep   int
 }
 
-func (o Octopi) String() string {
+func NewOctopi(data matrices.IntMatrix[int]) *Octopi {
+	return &Octopi{data: data}
+}
+
+func (o *Octopi) String() string {
 	out := ""
-	for _, line := range o {
+	for _, line := range o.data {
 		for _, val := range line {
 			out += strconv.Itoa(val)
 		}
@@ -65,43 +45,64 @@ func (o Octopi) String() string {
 	return out
 }
 
-func (o Octopi) ProgressStep() (flashes int, allFlashed bool) {
+func (o *Octopi) RunSimulation() {
+	// Keep progressing the octopi until we have at least reached 100 steps
+	// and we have seen all the octopi flash at the same time
+	syncFlashOccurred := false
+	totalFlashes := 0
+	for !(o.step > 100 && syncFlashOccurred) {
+		o.step++
+		stepFlashes, syncFlashed := o.progressStep()
+		totalFlashes += stepFlashes
+
+		if o.step == 100 {
+			o.flashesAfter100Steps = totalFlashes
+		}
+
+		if syncFlashed && !syncFlashOccurred {
+			syncFlashOccurred = true
+			o.firstSyncFlashStep = o.step
+		}
+	}
+}
+
+func (o *Octopi) progressStep() (flashes int, allFlashed bool) {
 	// Increase all energy levels by 1
-	o.m().IncrementAll()
+	o.data.IncrementAll()
 
 	// Check for flashes on all octopi. If a flash is detected, also check neighbouring octopi as their
 	// energy level will increase. Will also reset the octopi to 0 energy level if they do flash
 	total := 0
-	o.m().ForEach(func(x, y int, energy int) {
-		total += o.CheckFlash(x, y)
+	o.data.ForEach(func(x, y int, energy int) {
+		total += o.checkFlash(x, y)
 	})
-	return total, o.m().NumOfElements() == total
+	return total, o.data.NumOfElements() == total
 }
 
-func (o Octopi) CheckFlash(x, y int) int {
+func (o *Octopi) checkFlash(x, y int) int {
 	// Ignore point if already flashed or doesn't have enough energy
-	if o.m().Get(x, y) < 10 {
+	if o.data.Get(x, y) < 10 {
 		return 0
 	}
 
 	// Reset the octopus energy level as it has flashed
-	o.m().Set(x, y, 0)
+	o.data.Set(x, y, 0)
 
 	// This point flashes, so increment all neighbouring octopi (if they haven't flashed)
-	o.m().ForEachNeighbour(true, x, y, func(x1, y1 int) {
-		o.IncrementIfNotFlashed(x1, y1)
+	o.data.ForEachNeighbour(true, x, y, func(x1, y1 int) {
+		o.incrementIfNotFlashed(x1, y1)
 	})
 
 	// Since we have incremented the neighbours, check if they have now flashed
 	flashes := 1
-	o.m().ForEachNeighbour(true, x, y, func(x1, y1 int) {
-		flashes += o.CheckFlash(x1, y1)
+	o.data.ForEachNeighbour(true, x, y, func(x1, y1 int) {
+		flashes += o.checkFlash(x1, y1)
 	})
 	return flashes
 }
 
-func (o Octopi) IncrementIfNotFlashed(x, y int) {
-	if o.m().Get(x, y) != 0 {
-		o.m().Increment(x, y)
+func (o *Octopi) incrementIfNotFlashed(x, y int) {
+	if o.data.Get(x, y) != 0 {
+		o.data.Increment(x, y)
 	}
 }
