6cfc487594609dad3c8cab7a488b5532fab4cefa
Ben Ibinson <ben@ibinson.dev>
2021-12-20 19:34:28 +0000

    Cleanup

--- day18/main.go
+++ day18/main.go
@@ -9,42 +9,30 @@ import (
 func main() {
 	nums := fileparser.ReadTypedLines("day18/input.txt", NewSnailPair)
 
-	current := nums[0]
-	for i := 1; i < len(nums); i++ {
-		current = Add(current, nums[i])
-		Reduce(current)
-	}
-	fmt.Println(Magnitude(current))
+	sum := Sum(nums)
+	fmt.Println("[Part 1] Magnitude of sum of numbers is:", sum.Magnitude())
 
-	nums = fileparser.ReadTypedLines("day18/input.txt", NewSnailPair)
 	max := len(nums)
 	maxVal := 0
+	// Consider every permutation of each number
 	for i := 0; i < max; i++ {
 		for j := 0; j < max; j++ {
 			if i == j {
 				continue
 			}
-			numsI := fileparser.ReadTypedLines("day18/input.txt", NewSnailPair)
-			numsJ := fileparser.ReadTypedLines("day18/input.txt", NewSnailPair)
-			num := Add(numsI[i], numsJ[j])
-			Reduce(num)
-			val := Magnitude(num)
-			if val > maxVal {
+			// Check addition both ways
+			testij := nums[i].Add(nums[j])
+			if val := testij.Magnitude(); val > maxVal {
 				maxVal = val
 			}
 
-			numsI = fileparser.ReadTypedLines("day18/input.txt", NewSnailPair)
-			numsJ = fileparser.ReadTypedLines("day18/input.txt", NewSnailPair)
-			num = Add(numsJ[j], numsI[i])
-			Reduce(num)
-			val = Magnitude(num)
-			if val > maxVal {
+			testji := nums[j].Add(nums[i])
+			if val := testji.Magnitude(); val > maxVal {
 				maxVal = val
 			}
 		}
 	}
-	fmt.Println(maxVal)
-
+	fmt.Println("[Part 2] Maximum mangitude of adding 2 numbers: ", maxVal)
 }
 
 type SnailPair struct {
@@ -55,7 +43,13 @@ type SnailPair struct {
 	ParentPair *SnailPair
 }
 
+func (s *SnailPair) Clone() *SnailPair {
+	pairString := s.String()
+	return NewSnailPair(pairString)
+}
+
 func NewSnailPair(line string) *SnailPair {
+	// Create a binary tree representing the snail pair
 	var current *SnailPair
 	stack := []*SnailPair{}
 	pointer := "left"
@@ -98,92 +92,121 @@ func NewSnailPair(line string) *SnailPair {
 	return nil
 }
 
+func Sum(pairs []*SnailPair) *SnailPair {
+	var current *SnailPair
+	for _, s := range pairs {
+		if current == nil {
+			current = s
+			continue
+		}
+		current = current.Add(s)
+	}
+	return current
+}
+
+func (s *SnailPair) Add(a *SnailPair) *SnailPair {
+	result := &SnailPair{
+		LeftPair:  s.Clone(),
+		RightPair: a.Clone(),
+	}
+	result.LeftPair.ParentPair = result
+	result.RightPair.ParentPair = result
+	result.reduce()
+	return result
+}
+
+func (s *SnailPair) Magnitude() int {
+	left := s.LeftVal
+	if s.LeftPair != nil {
+		left = s.LeftPair.Magnitude()
+	}
+
+	right := s.RightVal
+	if s.RightPair != nil {
+		right = s.RightPair.Magnitude()
+	}
+	return (3 * left) + (2 * right)
+}
+
 func (s *SnailPair) String() string {
-	leftPart := ""
-	if s.LeftPair == nil {
-		leftPart = strconv.Itoa(s.LeftVal)
-	} else {
+	leftPart := strconv.Itoa(s.LeftVal)
+	if s.LeftPair != nil {
 		leftPart = s.LeftPair.String()
 	}
 
-	rightPart := ""
-
-	if s.RightPair == nil {
-		rightPart = strconv.Itoa(s.RightVal)
-	} else {
+	rightPart := strconv.Itoa(s.RightVal)
+	if s.RightPair != nil {
 		rightPart = s.RightPair.String()
 	}
 	return fmt.Sprintf("[%s,%s]", leftPart, rightPart)
 }
 
-func Reduce(s *SnailPair) {
-	noop := false
-	for !noop {
-		if Explode(s) {
+func (s *SnailPair) Depth() int {
+	current := s
+	depth := 0
+	for current.ParentPair != nil {
+		depth++
+		current = current.ParentPair
+	}
+	return depth
+}
+
+func (s *SnailPair) reduce() {
+	nothingToDo := false
+	for !nothingToDo {
+		if s.explode() {
 			continue
 		}
-		if Split(s) {
+		if s.split() {
 			continue
 		}
-		noop = true
+		nothingToDo = true
 	}
 }
 
-func Add(s1 *SnailPair, s2 *SnailPair) *SnailPair {
-	new := &SnailPair{
-		LeftPair:  s1,
-		RightPair: s2,
-	}
-	s1.ParentPair = new
-	s2.ParentPair = new
-	return new
-}
-
-func Explode(s *SnailPair) bool {
+func (s *SnailPair) explode() bool {
 	if s == nil {
 		return false
 	}
 
-	boom := Explode(s.LeftPair)
-	if boom {
+	// Try to explode left most pairs before right most
+	// If we have performed an explosion, exit immediately
+	if boom := s.LeftPair.explode(); boom {
 		return true
 	}
-	boom = Explode(s.RightPair)
-	if boom {
+	if boom := s.RightPair.explode(); boom {
 		return true
 	}
 
-	depth := 0
-	depthCurrent := s.ParentPair
-	for depthCurrent != nil {
-		depth++
-		depthCurrent = depthCurrent.ParentPair
-	}
-	if depth != 4 {
+	// Only explode pairs nested 4 times or more, i.e. depth >= 4
+	if s.Depth() < 4 {
 		return false
 	}
 
-	// Explode this pair
-	if s.LeftPair != nil || s.RightPair != nil {
-		panic("pairs detected greater than depth 4")
-	}
-
-	none := false
+	// Propogate the left hand side value to the next node with a value
+	atRoot := false
 	prev := s
 	current := s.ParentPair
+	// Move to parent, if the immediate left is a value, add it to that
 	if current.LeftPair == nil {
 		current.LeftVal += s.LeftVal
 	} else {
+		// Keep moving up the tree until we are
+		// at the root of the tree or come from a right branch
 		for current.LeftPair == prev {
 			if current.ParentPair == nil {
-				none = true
+				atRoot = true
 				break
 			} else {
 				prev = current
 				current = current.ParentPair
 			}
 		}
-		if !none {
+
+		// If we reached the root, we can't move any further left
+		if !atRoot {
+			// If the left has a value, use it, otherwise move down the
+			// right branch as far as possible
 			if current.LeftPair == nil {
 				current.LeftVal += s.LeftVal
 			} else {
@@ -196,7 +219,9 @@ func Explode(s *SnailPair) bool {
 		}
 	}
 
-	none = false
+	// Propogate the right hand side value to the next node with a value
+	// same logic as above but across the opposite side
+	atRoot = false
 	prev = s
 	current = s.ParentPair
 	if current.RightPair == nil {
@@ -204,14 +229,14 @@ func Explode(s *SnailPair) bool {
 	} else {
 		for current.RightPair == prev {
 			if current.ParentPair == nil {
-				none = true
+				atRoot = true
 				break
 			} else {
 				prev = current
 				current = current.ParentPair
 			}
 		}
-		if !none {
+		if !atRoot {
 			if current.RightPair == nil {
 				current.RightVal += s.RightVal
 			} else {
@@ -224,72 +249,51 @@ func Explode(s *SnailPair) bool {
 		}
 	}
 
-	// Explode it
+	// Explode the actually pair, by moving to the parent node
+	// and setting the corresponding value to 0 depending on
+	// if we came from the left or right side
 	parent := s.ParentPair
 	if parent.LeftPair == s {
 		parent.LeftVal = 0
 		parent.LeftPair = nil
-	}
-	if parent.RightPair == s {
+	} else {
 		parent.RightVal = 0
 		parent.RightPair = nil
 	}
 	return true
 }
 
-func Split(s *SnailPair) bool {
+func (s *SnailPair) split() bool {
 	if s == nil {
 		return false
 	}
 
-	plip := Split(s.LeftPair)
-	if plip {
+	// Attept to split the left pairs first
+	if rip := s.LeftPair.split(); rip {
 		return true
 	}
 
-	if s.LeftPair == nil && s.LeftVal >= 10 {
+	if s.LeftVal >= 10 {
 		newLeft := s.LeftVal / 2
 		newRight := s.LeftVal - newLeft
-		newPair := &SnailPair{
-			LeftVal:    newLeft,
-			RightVal:   newRight,
-			ParentPair: s,
-		}
+		newPair := &SnailPair{LeftVal: newLeft, RightVal: newRight, ParentPair: s}
 		s.LeftVal = 0
 		s.LeftPair = newPair
 		return true
 	}
 
-	if s.RightPair == nil && s.RightVal >= 10 {
+	// Now start the right pairs
+	if s.RightVal >= 10 {
 		newLeft := s.RightVal / 2
 		newRight := s.RightVal - newLeft
-		newPair := &SnailPair{
-			LeftVal:    newLeft,
-			RightVal:   newRight,
-			ParentPair: s,
-		}
+		newPair := &SnailPair{LeftVal: newLeft, RightVal: newRight, ParentPair: s}
 		s.RightVal = 0
 		s.RightPair = newPair
 		return true
 	}
 
-	plip = Split(s.RightPair)
-	if plip {
+	if rip := s.RightPair.split(); rip {
 		return true
 	}
 	return false
-}
-
-func Magnitude(s *SnailPair) int {
-	left := s.LeftVal
-	if s.LeftPair != nil {
-		left = Magnitude(s.LeftPair)
-	}
-
-	right := s.RightVal
-	if s.RightPair != nil {
-		right = Magnitude(s.RightPair)
-	}
-
-	return (3 * left) + (2 * right)
 }
